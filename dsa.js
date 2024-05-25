dac14800ea8fac65805edc7e2537e00c48b471ac
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-05-25 17:14:53 +0530

    feat: Add FibonacciNumber function

--- dsa.js
+++ dsa.js
@@ -5,3 +5,17 @@ function isPalandrome(x) {
   return x < 0 ? false : x === Number(x.toString().split("").reverse().join(""))
 }
 console.log(isPalandrome(121))
+
+//2. Fibonacci Number
+
+const fibArray = [0, 1];
+const numLength = 5;
+function FibonacciNumber(numLength) {
+  for (let i = 2; i <= numLength -1; i++) {
+    fibArray.push(fibArray[i-1] + fibArray[i-2]);  //first method 
+    // fibArray[i] = fibArray[i - 1] + fibArray[i - 2];   //second method 
+  }
+  return fibArray;
+}
+const data = FibonacciNumber(numLength);
+console.log(data);
