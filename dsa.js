937f431133988f22cb88f2a6d17d06795f0c8323
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-05-28 20:39:24 +0530

    find uniq number and also repated number

--- dsa.js
+++ dsa.js
@@ -1,13 +1,15 @@
 /** @format */
 
 //1. isPalandrome
+/*
 function isPalandrome(x) {
   return x < 0 ? false : x === Number(x.toString().split("").reverse().join(""))
 }
 console.log(isPalandrome(121))
+*/
 
 //2. Fibonacci Number
-
+/*
 const fibArray = [0, 1];
 const numLength = 5;
 function FibonacciNumber(numLength) {
@@ -19,13 +21,10 @@ function FibonacciNumber(numLength) {
 }
 const d = FibonacciNumber(numLength);
 console.log(d);
-
-
-
-
+*/
 
 //3. nth Fibonacci number
-
+/*
 const n = 10;
 function fib(n) {
  const fibArray = [0, 1];
@@ -36,3 +35,26 @@ function fib(n) {
 }
 const data = fib(n);
 console.log(data);
+*/
+
+
+
+//4. find uniq number and also repated number
+/*
+const nums = [5, 1, 3, 4, 2, 2, 5, 1]
+let uniqArr = []
+let repetNumber = []
+
+const repetNum = () => {
+  for (let i = 0; i <= nums.length - 1; i++) {
+    if (nums[i] !== uniqArr.find((num) => num == nums[i])) {
+      uniqArr.push(nums[i])
+    } else {
+      repetNumber.push(nums[i])
+    }
+  }
+  console.log("uniqArr: ", uniqArr)
+  console.log("repetNumber: ", repetNumber)
+}
+repetNum();
+*/
