bc4a0fbaf88db3b458bdccf78094d6451963fdcb
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-06-05 12:06:24 +0530

    how much letters in any array and set it in Obj

--- dsa.js
+++ dsa.js
@@ -3,33 +3,31 @@
 //1. isPalandrome  -------------------------------------------------------------------------
 
 function isPalandrome(x) {
-  return x < 0 ? false : x === Number(x.toString().split("").reverse().join(""))
+  return x < 0 ? false : x === Number(x.toString().split('').reverse().join(''));
 }
 // console.log(isPalandrome(121))
-
 
 //2. Fibonacci Number  ---------------------------------------------------------------------
 
 const fibArray = [0, 1];
 const numLength = 5;
 function FibonacciNumber(numLength) {
-  for (let i = 2; i <= numLength -1; i++) {
-    fibArray.push(fibArray[i-1] + fibArray[i-2]);  //first method 
-    // fibArray[i] = fibArray[i - 1] + fibArray[i - 2];   //second method 
+  for (let i = 2; i <= numLength - 1; i++) {
+    fibArray.push(fibArray[i - 1] + fibArray[i - 2]); //first method
+    // fibArray[i] = fibArray[i - 1] + fibArray[i - 2];   //second method
   }
   return fibArray;
 }
 const d = FibonacciNumber(numLength);
 // console.log(d);
 
-
 //3. nth Fibonacci number  ----------------------------------------------------------------
 
 const n = 10;
 function fib(n) {
- const fibArray = [0, 1];
+  const fibArray = [0, 1];
   for (let i = 2; i <= n; i++) {
-    fibArray.push(fibArray[i-1] + fibArray[i-2]);
+    fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
   }
   return fibArray[n];
 }
@@ -45,68 +43,61 @@ const fib2 = function (n) {
 };
 // console.log(fib(20));
 
-
-
-
-
 //4. find uniq number and also repated number  ----------------------------------------------
 
-const nums = [5, 1, 3, 4, 2, 2, 5, 1]
-let uniqArr = []
-let repetNumber = []
+const nums = [5, 1, 3, 4, 2, 2, 5, 1];
+let uniqArr = [];
+let repetNumber = [];
 
 const repetNum = () => {
   for (let i = 0; i <= nums.length - 1; i++) {
     if (nums[i] !== uniqArr.find((num) => num == nums[i])) {
-      uniqArr.push(nums[i])
+      uniqArr.push(nums[i]);
     } else {
-      repetNumber.push(nums[i])
+      repetNumber.push(nums[i]);
     }
   }
   // console.log("uniqArr: ", uniqArr)
   // console.log("repetNumber: ", repetNumber)
-}
+};
 repetNum();
-
 
 //5. isOppositeSame  -----------------------------------------------------------------------
 
-const isOpposite = function (a, b){
- return Array(...a).reverse().join("") === b
-}
-const res = isOpposite("satya", "ayas")
+const isOpposite = function (a, b) {
+  return (
+    Array(...a)
+      .reverse()
+      .join('') === b
+  );
+};
+const res = isOpposite('satya', 'ayas');
 // console.log(res)
-
 
 //6. isAnagram
 
-const isAnagtam = function (a, b){
-  return (a.split('').sort().join('') === b.split('').sort().join(''))
-}
+const isAnagtam = function (a, b) {
+  return a.split('').sort().join('') === b.split('').sort().join('');
+};
 // console.log(isAnagtam("satya", "ysata"))
-
 
 //7. find the second largest number  -------------------------------------------------------
 
-const arr = [19, 32,32, 23, 14, 33,33, 5]
+const arr = [19, 32, 32, 23, 14, 33, 33, 5];
 const secondLargestNum = function (x) {
-
   //first method
-  const data = x.sort((a, b) => a - b)
-  const lastNum = data[data.length - 1]
-  const secondLargestNumber = data.findLast((a) => a < lastNum)
-  console.log("secondLargestNumber: ", secondLargestNumber);
-
+  const data = x.sort((a, b) => a - b);
+  const lastNum = data[data.length - 1];
+  const secondLargestNumber = data.findLast((a) => a < lastNum);
+  console.log('secondLargestNumber: ', secondLargestNumber);
 
   //second method
-  const uniqArr = new Set(x) //remove duplicate
-  const sortedArr =  Array.from(uniqArr).sort((a, b) => a - b)
-  const secondLargestNum = sortedArr[sortedArr.length - 2]
+  const uniqArr = new Set(x); //remove duplicate
+  const sortedArr = Array.from(uniqArr).sort((a, b) => a - b);
+  const secondLargestNum = sortedArr[sortedArr.length - 2];
   console.log('secondLargestNum: ', secondLargestNum, typeof secondLargestNum);
-}
+};
 // secondLargestNum(arr)
-
-
 
 //8. rotate array from last nth number  -----------------------------------------------------
 
@@ -118,7 +109,6 @@ const oppositeFrom = function (arr1, nth) {
   console.log(arr1);
 };
 // oppositeFrom(arr1, 2);
-
 
 //9. objects in javascript practice --------------------------------------------------------
 
@@ -137,34 +127,28 @@ const students = [
 const greater20Age = students.filter((stu) => stu.age > 20);
 // console.log(greater20Age);
 
-
 // age greater than 20 and accending order
 const ageAccendingOrder = greater20Age.sort((a, b) => a.age > b.age);
 // console.log(ageAccendingOrder)
-
 
 // age greater then 20 and should be male
 const maleStudents = students.filter((stu) => stu.age > 20 && stu.gender === 'male');
 // console.log(maleStudents);
 
-
 //max age student
-const maxAgeStu = students.sort((a,b)=> a.age>b.age)[students.length-1]
+const maxAgeStu = students.sort((a, b) => a.age > b.age)[students.length - 1];
 // console.log(maxAgeStu)
 
-
 //max age male student
-const allMale = students.filter((stu)=> stu.gender === 'male') //all male student
-const accendingAge = allMale.sort((a,b)=> a.age > b.age)  // accending age order of all male student
+const allMale = students.filter((stu) => stu.gender === 'male'); //all male student
+const accendingAge = allMale.sort((a, b) => a.age > b.age); // accending age order of all male student
 // console.log(accendingAge[allMale.length -1 ]);
-
 
 //10. find the first max length of word on a sentence/---------------------------------------
 
-
-const sentence = 'Lorem ipsum dolor sit amet consecteturg adipisicing elitaiauwe'
-const maxLengthWord = sentence.split(' ').sort((a,b)=>b.length-a.length)[0]
-console.log(maxLengthWord, '-', maxLengthWord.length) 
+const sentence = 'Lorem ipsum dolor sit amet consecteturg adipisicing elitaiauwe';
+const maxLengthWord = sentence.split(' ').sort((a, b) => b.length - a.length)[0];
+// console.log(maxLengthWord, '-', maxLengthWord.length);
 
 //second method
 const senArray = sentence.split(' ');
@@ -173,15 +157,14 @@ const maxLengthWord2 = senArray.reduce((longentWord, curr) => {
 });
 // console.log(maxLengthWord2);
 
-
-
 //In sentence all words first letter should be capitilize------------------------------------
 
 const st = 'i am satya divedi and i am from india.';
-const stArr = st.split(' ').map((word) => word.charAt(0).toUpperCase().concat(word.slice(1))).join(' ');
-console.log(stArr);
-
-
+const stArr = st
+  .split(' ')
+  .map((word) => word.charAt(0).toUpperCase().concat(word.slice(1)))
+  .join(' ');
+// console.log(stArr);
 
 // count how much time the letter is printed on a word-----------------------------------------------
 
@@ -194,6 +177,30 @@ const countRepetLetterInWord = word.split('').reduce((acc, curr) => {
   }
   return acc;
 }, 0);
-
 // console.log(countRepetLetterInWord);
 
+
+//count how much letters in any array and set it in Obj-----------------------------------------------
+
+let array = ['f','a', 'b', 'a', 'c', 'b', 'a', 'd', 'b','f'];
+const obj = array.reduce((acc, curr) => {
+  if (curr in acc) {
+    console.log(` ${curr} mil gya aur bda diya 1`)
+    acc[curr]++;
+  } else {
+    console.log(` ${curr} nai mila to dal diya 1 `)
+    acc[curr] = 1;
+  }
+  return acc;
+}, {});
+// console.log(obj);
+
+let obj2 = {};
+array.forEach((letter) => {
+  if (letter in obj2) {
+    obj2[letter]++;
+  } else {
+    obj2[letter] = 1;
+  }
+});
+// console.log(obj2);
