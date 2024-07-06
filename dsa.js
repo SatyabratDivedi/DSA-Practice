105cd1736187a9238676b04223b4493c0981b031
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 15:35:41 +0530

    3. Logic-1 > squirrelPlay

--- dsa.js
+++ dsa.js
@@ -316,3 +316,32 @@ console.log(dateFashion(5, 2));
 console.log(dateFashion(5, 5));
 console.log(dateFashion(10, 2));
 console.log(dateFashion(2, 9));
+
+
+// 3. Logic-1 > squirrelPlay----------------------------------------------------------------------------------
+
+/*
+The squirrels in Palo Alto spend most of the day playing. 
+In particular, they play if the temperature is between 60 and 90 (inclusive). 
+Unless it is summer, then the upper limit is 100 instead of 90. 
+Given an int temperature and a boolean isSummer, 
+return true if the squirrels play and false otherwise.
+*/
+
+// squirrelPlay(70, false) → true
+// squirrelPlay(95, false) → false
+// squirrelPlay(95, true) → true
+
+function squirrelPlay(temp, isSummer) {
+  if (isSummer && temp >= 60 && temp <= 100) {
+    return true;
+  } else if (temp >= 60 && temp <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(squirrelPlay(70, false))
+console.log(squirrelPlay(95, false))
+console.log(squirrelPlay(95, true))
