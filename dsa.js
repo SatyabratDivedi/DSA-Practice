c3e153ae1f756580c60c187f30a77ead0ca79243
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 14:38:19 +0530

    Logic-1 > cigarParty

--- dsa.js
+++ dsa.js
@@ -3,7 +3,7 @@
 //1. isPalandrome  -------------------------------------------------------------------------
 
 function isPalandrome(x) {
-  return x < 0 ? false : x === Number(x.toString().split('').reverse().join(''));
+  return x < 0 ? false : x === Number(x.toString().split("").reverse().join(""));
 }
 // console.log(isPalandrome(121))
 
@@ -68,16 +68,16 @@ const isOpposite = function (a, b) {
   return (
     Array(...a)
       .reverse()
-      .join('') === b
+      .join("") === b
   );
 };
-const res = isOpposite('satya', 'ayas');
+const res = isOpposite("satya", "ayas");
 // console.log(res)
 
 //6. isAnagram
 
 const isAnagtam = function (a, b) {
-  return a.split('').sort().join('') === b.split('').sort().join('');
+  return a.split("").sort().join("") === b.split("").sort().join("");
 };
 // console.log(isAnagtam("satya", "ysata"))
 
@@ -89,22 +89,22 @@ const secondLargestNum = function (x) {
   const data = x.sort((a, b) => a - b);
   const lastNum = data[data.length - 1];
   const secondLargestNumber = data.findLast((a) => a < lastNum);
-  console.log('secondLargestNumber: ', secondLargestNumber);
+  console.log("secondLargestNumber: ", secondLargestNumber);
 
   //second method
   const uniqArr = new Set(x); //remove duplicate
   const sortedArr = Array.from(uniqArr).sort((a, b) => a - b);
   const secondLargestNum = sortedArr[sortedArr.length - 2];
-  console.log('secondLargestNum: ', secondLargestNum, typeof secondLargestNum);
+  console.log("secondLargestNum: ", secondLargestNum, typeof secondLargestNum);
 };
 // secondLargestNum(arr)
 
 //8. rotate array from last nth number  -----------------------------------------------------
 
-let arr1 = ['1', '2', '3', '4', '5', '6', '7'];
+let arr1 = ["1", "2", "3", "4", "5", "6", "7"];
 const oppositeFrom = function (arr1, nth) {
   const lastNthNum = arr1.splice(arr1.length - nth);
-  console.log('lastNthNum: ', lastNthNum);
+  console.log("lastNthNum: ", lastNthNum);
   arr1.unshift(...lastNthNum);
   console.log(arr1);
 };
@@ -113,14 +113,14 @@ const oppositeFrom = function (arr1, nth) {
 //9. objects in javascript practice --------------------------------------------------------
 
 const students = [
-  {name: 'neha', age: 44, gender: 'female'},
-  {name: 'satya', age: 20, gender: 'male'},
-  {name: 'shubham', age: 22, gender: 'male'},
-  {name: 'shivam', age: 21, gender: 'male'},
-  {name: 'priyanshi', age: 18, gender: 'female'},
-  {name: 'raginee', age: 28, gender: 'female'},
-  {name: 'saumya', age: 4, gender: 'female'},
-  {name: 'lala', age: 36, gender: 'male'},
+  {name: "neha", age: 44, gender: "female"},
+  {name: "satya", age: 20, gender: "male"},
+  {name: "shubham", age: 22, gender: "male"},
+  {name: "shivam", age: 21, gender: "male"},
+  {name: "priyanshi", age: 18, gender: "female"},
+  {name: "raginee", age: 28, gender: "female"},
+  {name: "saumya", age: 4, gender: "female"},
+  {name: "lala", age: 36, gender: "male"},
 ];
 
 //all students whose age is greater than 20
@@ -132,7 +132,7 @@ const ageAccendingOrder = greater20Age.sort((a, b) => a.age > b.age);
 // console.log(ageAccendingOrder)
 
 // age greater then 20 and should be male
-const maleStudents = students.filter((stu) => stu.age > 20 && stu.gender === 'male');
+const maleStudents = students.filter((stu) => stu.age > 20 && stu.gender === "male");
 // console.log(maleStudents);
 
 //max age student
@@ -140,18 +140,18 @@ const maxAgeStu = students.sort((a, b) => a.age > b.age)[students.length - 1];
 // console.log(maxAgeStu)
 
 //max age male student
-const allMale = students.filter((stu) => stu.gender === 'male'); //all male student
+const allMale = students.filter((stu) => stu.gender === "male"); //all male student
 const accendingAge = allMale.sort((a, b) => a.age > b.age); // accending age order of all male student
 // console.log(accendingAge[allMale.length -1 ]);
 
 //10. find the first max length of word on a sentence/---------------------------------------
 
-const sentence = 'Lorem ipsum dolor sit amet consecteturg adipisicing elitaiauwe';
-const maxLengthWord = sentence.split(' ').sort((a, b) => b.length - a.length)[0];
+const sentence = "Lorem ipsum dolor sit amet consecteturg adipisicing elitaiauwe";
+const maxLengthWord = sentence.split(" ").sort((a, b) => b.length - a.length)[0];
 // console.log(maxLengthWord, '-', maxLengthWord.length);
 
 //second method
-const senArray = sentence.split(' ');
+const senArray = sentence.split(" ");
 const maxLengthWord2 = senArray.reduce((longentWord, curr) => {
   return longentWord.length < curr.length ? curr : longentWord;
 });
@@ -159,19 +159,19 @@ const maxLengthWord2 = senArray.reduce((longentWord, curr) => {
 
 //11. In sentence all words first letter should be capitilize------------------------------------
 
-const st = 'i am satya divedi and i am from india.';
+const st = "i am satya divedi and i am from india.";
 const stArr = st
-  .split(' ')
+  .split(" ")
   .map((word) => word.charAt(0).toUpperCase().concat(word.slice(1)))
-  .join(' ');
+  .join(" ");
 // console.log(stArr);
 
 //12. count how much time the letter is printed on a word-----------------------------------------------
 
-let word = 'pinnapllan';
-let repetLetter = 'l';
+let word = "pinnapllan";
+let repetLetter = "l";
 
-const countRepetLetterInWord = word.split('').reduce((acc, curr) => {
+const countRepetLetterInWord = word.split("").reduce((acc, curr) => {
   if (curr == repetLetter) {
     acc++;
   }
@@ -179,10 +179,9 @@ const countRepetLetterInWord = word.split('').reduce((acc, curr) => {
 }, 0);
 // console.log(countRepetLetterInWord);
 
-
 //13. count how much letters in any array and set it in Obj-----------------------------------------------
 
-let array = ['f','a', 'b', 'a', 'c', 'b', 'a', 'd', 'b','f'];
+let array = ["f", "a", "b", "a", "c", "b", "a", "d", "b", "f"];
 const obj = array.reduce((acc, curr) => {
   if (curr in acc) {
     acc[curr]++;
@@ -207,48 +206,78 @@ array.forEach((letter) => {
 //14. reverse a string------------------------------------------------------------------------------
 
 function reverseString(str) {
-    let reversedStr = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-      reversedStr += str[i];
-    }
-    return reversedStr;
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 }
 // console.log(reverseString('uhelloo'));
 
-
 //15. store unique value from an array----------------------------------------------------------------
 
-const arrForUniq = [11, 2, 11, 3, 2, 1, 37, 11, 44, 5, 7, 5, 11]
+const arrForUniq = [11, 2, 11, 3, 2, 1, 37, 11, 44, 5, 7, 5, 11];
 //firse mathod
 const uniqueArr = () => {
   const uni = arrForUniq.filter((num, index, self) => {
-    return self.indexOf(num) === index
-  })
-  console.log(uni)
-}
+    return self.indexOf(num) === index;
+  });
+  console.log(uni);
+};
 // uniqueArr()
 
 //second mathod
 const uniqArray = () => {
-  const uniqArr = [arrForUniq[0]]
+  const uniqArr = [arrForUniq[0]];
   for (let i = 1; i <= arrForUniq.length - 1; i++) {
-    const isAlready = uniqArr.indexOf(arrForUniq[i])
+    const isAlready = uniqArr.indexOf(arrForUniq[i]);
     if (isAlready == -1) {
-      uniqArr.push(arrForUniq[i])
+      uniqArr.push(arrForUniq[i]);
     }
   }
-  console.log(uniqArr)
-}
-uniqArray()
-
+  console.log(uniqArr);
+};
+uniqArray();
 
 //16. isAnyNumRepeted--------------------------------------------------------------------------------------
 
-var arr = [1, 2, 2, 1, 1, 3] //=> true
-var arr2 = [1, 2, 1] //=> true
-var arr3 = [1, 2, 3,4,5] //=> false
+var arr = [1, 2, 2, 1, 1, 3]; //=> true
+var arr2 = [1, 2, 1]; //=> true
+var arr3 = [1, 2, 3, 4, 5]; //=> false
 
 var isRepetedLetter = function (recArr) {
-  return [...new Set(recArr)].length !== recArr.length
+  return [...new Set(recArr)].length !== recArr.length;
+};
+console.log(isRepetedLetter(arr3));
+
+// here i am stating dsa which is given by cuvette course
+
+// 1. Logic-1 > cigarParty----------------------------------------------------------------------------------
+
+/*When squirrels get together for a party, they like to have cigars.
+ A squirrel party is successful when the number of 
+ cigars is between 40 and 60, inclusive. Unless it is the weekend,
+  in which case there is no upper bound on the number of cigars. 
+  Return true if the party with the given values is successful, 
+  or false otherwise*/
+
+// cigarParty(30, false) → false
+// cigarParty(50, false); → true
+// cigarParty(70, true); → true
+// cigarParty(30, true) → false
+// cigarParty(50, true) → true
+
+function cigarParty(cigars, isWeekend) {
+  if (isWeekend && cigars >= 40) {
+    return true;
+  } else if (!isWeekend && cigars >= 40 && cigars <= 60) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(isRepetedLetter(arr3))
+
+console.log(cigarParty(30, false));
+console.log(cigarParty(50, false));
+console.log(cigarParty(70, true));
+console.log(cigarParty(30, true));
