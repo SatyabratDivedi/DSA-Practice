66e532f30029b256ffe1c7f7f7df604e5f7d5474
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 17:42:44 +0530

    4. Logic-1 > sortaSum

--- dsa.js
+++ dsa.js
@@ -282,8 +282,6 @@ console.log(cigarParty(50, false));
 console.log(cigarParty(70, true));
 console.log(cigarParty(30, true));
 
-
-
 // 2. Logic-1 > dateFashion----------------------------------------------------------------------------------
 
 /*
@@ -317,7 +315,6 @@ console.log(dateFashion(5, 5));
 console.log(dateFashion(10, 2));
 console.log(dateFashion(2, 9));
 
-
 // 3. Logic-1 > squirrelPlay----------------------------------------------------------------------------------
 
 /*
@@ -342,6 +339,29 @@ function squirrelPlay(temp, isSummer) {
   }
 }
 
-console.log(squirrelPlay(70, false))
-console.log(squirrelPlay(95, false))
-console.log(squirrelPlay(95, true))
+console.log(squirrelPlay(70, false));
+console.log(squirrelPlay(95, false));
+console.log(squirrelPlay(95, true));
+
+// 4. Logic-1 > sortaSum----------------------------------------------------------------------------------
+
+/*
+Given 2 ints, a and b, return their sum. 
+However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+*/
+
+// sortaSum(3, 4) → 7
+// sortaSum(9, 4) → 20
+// sortaSum(10, 11) → 21
+
+function sortaSum(a, b) {
+  if (a + b >= 10 && a + b <= 19) {
+    return 20;
+  } else {
+    return a + b;
+  }
+}
+
+console.log(sortaSum(3, 4));
+console.log(sortaSum(9, 4));
+console.log(sortaSum(10, 11));
