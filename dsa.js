9abff9e142ee4adc2aeb985f403b021c69951435
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 19:03:32 +0530

    9. Logic-1 > more20

--- dsa.js
+++ dsa.js
@@ -453,3 +453,21 @@ function specialEleven(n) {
 console.log(specialEleven(22));
 console.log(specialEleven(23));
 console.log(specialEleven(24));
+
+// 9. Logic-1 > more20----------------------------------------------------------------------------------
+
+/*
+Return true if the given non-negative number is 1 or 2 more than a multiple of 20
+*/
+
+// more20(20) → false
+// more20(21) → true
+// more20(22) → true
+
+function more20(n) {
+  return n%20 == 1 || n%20 ==2;
+}
+
+console.log(more20(20))
+console.log(more20(21))
+console.log(more20(22))
