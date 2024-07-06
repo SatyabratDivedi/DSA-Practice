33d71fd37a5c93fcf3f0762f7e94f8ed1543173a
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 19:12:30 +0530

    10. Logic-1 > old35

--- dsa.js
+++ dsa.js
@@ -471,3 +471,22 @@ function more20(n) {
 console.log(more20(20))
 console.log(more20(21))
 console.log(more20(22))
+
+// 10. Logic-1 > old35----------------------------------------------------------------------------------
+
+/*
+Return true if the given non-negative number is a multiple of 3 or 5, but not both.
+*/
+
+// old35(3) → true
+// old35(10) → true
+// old35(15) → false
+
+function old35(n) {
+  if (n % 3 == 0 && n % 5 == 0) return false;
+  return n % 3 == 0 || n % 5 == 0;
+}
+
+console.log(old35(3));
+console.log(old35(10));
+console.log(old35(15));
