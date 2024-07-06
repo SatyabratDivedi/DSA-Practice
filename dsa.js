50c1efbb5e927e89f8268a9affad8c63d345ced1
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 18:44:03 +0530

    7. Logic-1 > in1To10

--- dsa.js
+++ dsa.js
@@ -409,3 +409,28 @@ function love6(a, b) {
 console.log(love6(6, 4));
 console.log(love6(4, 5));
 console.log(love6(1, 5));
+
+
+// 7. Logic-1 > in1To10----------------------------------------------------------------------------------
+
+/*
+Given a number n, return true if n is in the range 1..10, inclusive. 
+Unless outsideMode is true, in which case return true if the number is less or
+equal to 1, or greater or equal to 10.
+*/
+
+// in1To10(5, false) → true
+// in1To10(11, false) → false
+// in1To10(11, true) → true
+
+function in1To10(n, outsideMode) {
+  if (outsideMode) {
+    return n <= 1 || n >= 10;
+  } else {
+    return n >= 1 && n <= 10;
+  }
+}
+
+console.log(in1To10(5, false))
+console.log(in1To10(11, false))
+console.log(in1To10(11, true))
