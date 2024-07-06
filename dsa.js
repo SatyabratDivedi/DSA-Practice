8d7ec5d775ecf54b1d99725b993616b14430d5fe
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 18:56:54 +0530

    8. Logic-1 > specialEleven

--- dsa.js
+++ dsa.js
@@ -434,3 +434,22 @@ function in1To10(n, outsideMode) {
 console.log(in1To10(5, false))
 console.log(in1To10(11, false))
 console.log(in1To10(11, true))
+
+// 8. Logic-1 > specialEleven----------------------------------------------------------------------------------
+
+/*
+We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+Return true if the given non-negative number is special.
+*/
+
+// specialEleven(22) → true
+// specialEleven(23) → true
+// specialEleven(24) → false
+
+function specialEleven(n) {
+  return n % 11 == 0 || n % 11 == 1;
+}
+
+console.log(specialEleven(22));
+console.log(specialEleven(23));
+console.log(specialEleven(24));
