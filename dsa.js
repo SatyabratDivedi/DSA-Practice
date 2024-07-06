bf83616041d306040042adf4a8a33740e45fa9d9
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 14:48:39 +0530

    2. Logic-1 > dateFashion

--- dsa.js
+++ dsa.js
@@ -281,3 +281,38 @@ console.log(cigarParty(30, false));
 console.log(cigarParty(50, false));
 console.log(cigarParty(70, true));
 console.log(cigarParty(30, true));
+
+
+
+// 2. Logic-1 > dateFashion----------------------------------------------------------------------------------
+
+/*
+You and your date are trying to get a table at a restaurant.
+ The parameter "you" is the stylishness of your clothes,
+  in the range 0..10, and "date" is the stylishness of your date's clothes.
+   The result getting the table is encoded as an int value with 0=no, 
+   1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the 
+   result is 2 (yes). With the exception that if either of you has style of 
+   2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
+*/
+// dateFashion(5, 10) → 2
+// dateFashion(5, 2) → 0
+// dateFashion(5, 5) → 1
+// dateFashion(10, 2) → 0
+// dateFashion(2, 9) → 0
+
+function dateFashion(you, date) {
+  if ((you >= 8 && date > 2) || (date >= 8 && you > 2)) {
+    return 2;
+  } else if (you <= 2 || date <= 2) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+console.log(dateFashion(5, 10));
+console.log(dateFashion(5, 2));
+console.log(dateFashion(5, 5));
+console.log(dateFashion(10, 2));
+console.log(dateFashion(2, 9));
