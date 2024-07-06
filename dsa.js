0297d1d1997b986866e742e523d910161cb5ca09
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-06 18:12:27 +0530

    5. Logic-1 > alarmClock

--- dsa.js
+++ dsa.js
@@ -365,3 +365,36 @@ function sortaSum(a, b) {
 console.log(sortaSum(3, 4));
 console.log(sortaSum(9, 4));
 console.log(sortaSum(10, 11));
+
+
+// 5. Logic-1 > alarmClock----------------------------------------------------------------------------------
+
+
+/*
+
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, 
+and a boolean indicating if we are on vacation, 
+return a string of the form "7:00" indicating when the alarm clock 
+should ring. Weekdays, the alarm should be "7:00" 
+and on the weekend it should be "10:00". 
+Unless we are on vacation -- then on weekdays it should be 
+"10:00" and weekends it should be "off".
+*/
+
+// alarmClock(1, false) → "7:00"
+// alarmClock(5, false) → "7:00"
+// alarmClock(0, false) → "10:00"
+// alarmClock(0, true) → "off"
+
+function alarmClock(day, vacation) {
+  if (vacation) {
+    return day === 0 || day === 6 ? "off" : "10:00";
+  } else {
+    return day === 0 || day === 6 ? "10:00" : "7:00";
+  }
+}
+
+console.log(alarmClock(1, false));
+console.log(alarmClock(5, false));
+console.log(alarmClock(0, false));
+console.log(alarmClock(0, true));
