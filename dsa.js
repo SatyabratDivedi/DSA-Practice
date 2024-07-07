ccd569a7acb979a17f8087f8014498ca17b0a6b6
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-07 19:09:48 +0530

    19. String-1 > makeTags

--- dsa.js
+++ dsa.js
@@ -727,3 +727,22 @@ function makeAbba(a, b) {
 console.log(makeAbba("Hi", "Bye"))
 console.log(makeAbba("Yo", "Alice"))
 console.log(makeAbba("What", "Up"))
+
+// 19. String-1 > makeTags---------------------------------------------------------------------------------
+/*
+The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text.In this example, the "i" tag makes <i> and </i> which surround the word "Yay". 
+Given tag and word strings, create the HTML string with tags around the word, e.g. <i>Yay</i>".
+*/
+
+// makeTags("i", "Yay") → "<i>Yay</i>"
+// makeTags("i", "Hello") → "<i>Hello</i>"
+// makeTags("cite", "Yay") → "<cite>Yay</cite>"
+
+
+function  makeTags(tag, word) {
+  return `<${tag}>${word}</${tag}>`;
+}
+
+console.log(makeTags("i", "Yay"))
+console.log(makeTags("i", "Hello"))
+console.log(makeTags("cite", "Yay"))
