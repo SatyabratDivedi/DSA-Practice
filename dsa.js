e48c31fb0e8cdbd603df8d39f7237057573d7803
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-08 11:49:38 +0530

    24. String-2 > countCode

--- dsa.js
+++ dsa.js
@@ -839,3 +839,29 @@ function countHi(str) {
 console.log(countHi("abc hi ho"));
 console.log(countHi("ABChi hi"));
 console.log(countHi("hihi"));
+
+
+// 24. String-2 > countCode---------------------------------------------------------------------------------
+
+/*
+Return the number of times that the string "code" appears anywhere in the given string, 
+except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+*/
+
+// countCode("aaacodebbb") → 1
+// countCode("codexxcode") → 2
+// countCode("cozexxcope") → 2
+
+function countCode(str) {
+  let count = 0;
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (str[i] == "c" && str[i + 1] == "o" && str[i + 3] == "e") {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countCode("aaacodebbb"));
+console.log(countCode("codexxcode"));
+console.log(countCode("cozexxcope"));
