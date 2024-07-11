a9fea8b00daa744031f0723b27df6ff4949a8f41
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-07-11 12:21:37 +0530

    36. String-2 > withoutString

--- dsa.js
+++ dsa.js
@@ -1134,3 +1134,27 @@ function countYZ(str) {
 console.log(countYZ("fez day"));
 console.log(countYZ("day fez"));
 console.log(countYZ("day fyyyz"));
+
+
+// 36. String-2 > withoutString---------------------------------------------------------------------------------
+
+/*
+Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+*/
+
+// withoutString("Hello there", "llo") → "He there"
+// withoutString("Hello there", "e") → "Hllo thr"
+// withoutString("Hello there", "x") → "Hello there"
+
+function withoutString(base, remove) {
+  remove = remove.toLowerCase();
+  if(remove.length>=3 && remove[0]===remove[1] && remove[1]===remove[2]){
+    remove = remove[0]+remove[1];
+    console.log(remove)
+  }
+  return base.replaceAll(remove, "");
+}
+
+console.log(withoutString("Hello therlloe", "llo"));
+console.log(withoutString("Hello there", "e"));
+console.log(withoutString("xHelxlo thexre", "x"));
